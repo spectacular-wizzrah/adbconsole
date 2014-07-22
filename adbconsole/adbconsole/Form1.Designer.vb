2394b66f7bafcc1c978a5c81bf5c237127f653be
wizzrah <wizzrah@outlook.com>
2014-07-22 16:17:34 -0700

    All files created by Visual Studio...
    This is everything I created and used (including the icons and
    adb/fastboot files!)

---
+++ adbconsole/adbconsole/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class console
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(console))
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.startbutton = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.BackColor = System.Drawing.Color.White
+        Me.OutputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.OutputTextBox.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OutputTextBox.ForeColor = System.Drawing.Color.Black
+        Me.OutputTextBox.Location = New System.Drawing.Point(12, 31)
+        Me.OutputTextBox.Multiline = True
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.OutputTextBox.Size = New System.Drawing.Size(482, 234)
+        Me.OutputTextBox.TabIndex = 22
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.InputTextBox.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InputTextBox.Location = New System.Drawing.Point(12, 271)
+        Me.InputTextBox.Multiline = True
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(404, 20)
+        Me.InputTextBox.TabIndex = 23
+        '
+        'startbutton
+        '
+        Me.startbutton.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.startbutton.FlatAppearance.BorderColor = System.Drawing.Color.Silver
+        Me.startbutton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.startbutton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.startbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.startbutton.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.startbutton.ForeColor = System.Drawing.Color.White
+        Me.startbutton.Location = New System.Drawing.Point(422, 269)
+        Me.startbutton.Name = "startbutton"
+        Me.startbutton.Size = New System.Drawing.Size(72, 23)
+        Me.startbutton.TabIndex = 24
+        Me.startbutton.Text = "Execute"
+        Me.startbutton.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI Semibold", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(144, Byte), Integer), CType(CType(144, Byte), Integer), CType(CType(144, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(7, 5)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(165, 25)
+        Me.Label6.TabIndex = 27
+        Me.Label6.Text = "ADB Console v1.0"
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Transparent
+        Me.Button9.BackgroundImage = CType(resources.GetObject("Button9.BackgroundImage"), System.Drawing.Image)
+        Me.Button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button9.FlatAppearance.BorderSize = 0
+        Me.Button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button9.Location = New System.Drawing.Point(499, 12)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(30, 30)
+        Me.Button9.TabIndex = 28
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.Transparent
+        Me.Button10.BackgroundImage = CType(resources.GetObject("Button10.BackgroundImage"), System.Drawing.Image)
+        Me.Button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button10.FlatAppearance.BorderSize = 0
+        Me.Button10.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(48, Byte), Integer))
+        Me.Button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button10.Location = New System.Drawing.Point(499, 48)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(30, 30)
+        Me.Button10.TabIndex = 29
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'console
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(541, 303)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.startbutton)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "console"
+        Me.Text = "ADB Console by wizzrah"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OutputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents InputTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents startbutton As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+
+End Class
---
+++ adbconsole/adbconsole/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class console
+    Private WithEvents MyProcess As Process
+    Private Delegate Sub AppendOutputTextDelegate(ByVal text As String)
+
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Me.WindowState = FormWindowState.Minimized
+
+    End Sub
+
+
+    Private Sub console_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AcceptButton = startbutton
+        MyProcess = New Process
+        With MyProcess.StartInfo
+            .FileName = "cmd.exe"
+            .UseShellExecute = False
+            .CreateNoWindow = True
+            .RedirectStandardInput = True
+            .RedirectStandardOutput = True
+            .RedirectStandardError = True
+        End With
+        MyProcess.Start()
+
+        MyProcess.BeginErrorReadLine()
+        MyProcess.BeginOutputReadLine()
+        AppendOutputText("Thanks for trying out ADB Console by wizzrah! " & MyProcess.StartTime.ToString)
+    End Sub
+
+    Private Sub MyProcess_ErrorDataReceived(ByVal sender As Object, ByVal e As System.Diagnostics.DataReceivedEventArgs) Handles MyProcess.ErrorDataReceived
+        AppendOutputText(vbCrLf & "Error: " & e.Data)
+    End Sub
+
+    Private Sub MyProcess_OutputDataReceived(ByVal sender As Object, ByVal e As System.Diagnostics.DataReceivedEventArgs) Handles MyProcess.OutputDataReceived
+        AppendOutputText(vbCrLf & e.Data)
+    End Sub
+
+    Private Sub startbutton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles startbutton.Click
+        MyProcess.StandardInput.WriteLine(InputTextBox.Text)
+        MyProcess.StandardInput.Flush()
+        InputTextBox.Text = ""
+    End Sub
+
+    Private Sub AppendOutputText(ByVal text As String)
+        If OutputTextBox.InvokeRequired Then
+            Dim myDelegate As New AppendOutputTextDelegate(AddressOf AppendOutputText)
+            Me.Invoke(myDelegate, text)
+        Else
+            OutputTextBox.AppendText(text)
+        End If
+    End Sub
+
+    Private mPrevPos As New Point
+    Private Sub console_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        Dim delta As New Size(e.X - mPrevPos.X, e.Y - mPrevPos.Y)
+        If (e.Button = MouseButtons.Left) Then
+            Me.Location += delta
+            mPrevPos = e.Location - delta
+        Else
+            mPrevPos = e.Location
+        End If
+    End Sub
+
+    Private Sub OutputTextBox_TextChanged(sender As Object, e As EventArgs) Handles OutputTextBox.TextChanged
+
+    End Sub
+
+    Private Sub InputTextBox_TextChanged(sender As Object, e As EventArgs) Handles InputTextBox.TextChanged
+
+    End Sub
+End Class
